117d589c935ebacf86ee27c6b584a5bfdbcf4ab5
paul-maher <paul.maher@open.ac.uk>
2019-06-11 19:42:00 +0000

    Tidied up moat creation and external fence

--- castle.py
+++ castle.py
@@ -27,7 +27,8 @@ def drawTower(x,y,z):
 	#	print x,y+11,n 
         #        mc.setBlock(x,y+11,n,block.STONE_BRICK)
         #        mc.setBlock(x+6,y+11,n,block.STONE_BRICK)
-        
+
+# Draw a wall in either the x or z direction        
 def drawWall(x,y,z,direction):
 
         if direction==1:
@@ -35,10 +36,9 @@ def drawWall(x,y,z,direction):
         else:
                 mc.setBlocks(x,y,z,x,y+7,z+11,block.STONE_BRICK)
 
+# Draw a castle, consisting of 4 towers, and 4 walls.
 def drawCastle(x,y,z):
 
-        #mc.setBlocks(x,y,z,x+22,y,z+22,block.COBBLESTONE)
-        
         drawTower(x,y,z)
         drawTower(x+17,y,z)
         drawTower(x,y,z+17)
@@ -49,57 +49,74 @@ def drawCastle(x,y,z):
         drawWall(x+1,y,z+7,0)
         drawWall(x+22,y,z+7,0)
 
-def drawFence(xPos,zPos):
-	for n in range(xPos,xPos+30):
+# Draw a lenght of fence. Note that we look to see the height of the terrain and 
+# build the fance at that height. Can get a bit wierd if there is a tree in the way!
+#def drawFence(xPos,zPos):
+
+	# How big of a fence?
+#	size = 39
+
+	# Draw the x direction fence
+#	for n in range(xPos,xPos+size):
+#		y = mc.getHeight(n,zPos)
+#        	mc.setBlock(n,y,zPos,block.FENCE)
+#        	mc.setBlock(n,y+1,zPos,block.FENCE)
+#		y = mc.getHeight(n,zPos+size)
+#        	mc.setBlock(n,y,zPos+size,block.FENCE)
+#        	mc.setBlock(n,y+1,zPos+size,block.FENCE)
+
+	# Then the z direction fence
+#	for n in range(zPos,zPos+size):
+#		y = mc.getHeight(xPos,n)
+#        	mc.setBlock(xPos,y,n,block.FENCE)
+#        	mc.setBlock(xPos,y+1,n,block.FENCE)
+#		y = mc.getHeight(xPos+size,n)
+#        	mc.setBlock(xPos+size,y,n,block.FENCE)
+#        	mc.setBlock(xPos+size,y+1,n,block.FENCE)
+
+def drawFenceEx(xPos,zPos):
+
+	# How big of a fence?
+	size = 39
+	height = 2
+
+	# Draw the x direction fence
+	for n in range(xPos,xPos+size):
 		y = mc.getHeight(n,zPos)
-        	mc.setBlock(n,y,zPos,block.FENCE)
-        	mc.setBlock(n,y+1,zPos,block.FENCE)
-		y = mc.getHeight(n,zPos+30)
-        	mc.setBlock(n,y,zPos+30,block.FENCE)
-        	mc.setBlock(n,y+1,zPos+30,block.FENCE)
+        	mc.setBlocks(n,y,zPos,n,y+height,zPos,block.FENCE)
+		y = mc.getHeight(n,zPos+size)
+        	mc.setBlocks(n,y,zPos+size,n,y+height,zPos+size,block.FENCE)
 
-	for n in range(zPos,zPos+30):
+	# Then the z direction fence
+	for n in range(zPos,zPos+size):
 		y = mc.getHeight(xPos,n)
-        	mc.setBlock(xPos,y,n,block.FENCE)
-        	mc.setBlock(xPos,y+1,n,block.FENCE)
-		y = mc.getHeight(xPos+30,n)
-        	mc.setBlock(xPos+30,y,n,block.FENCE)
-        	mc.setBlock(xPos+30,y+1,n,block.FENCE)
+        	mc.setBlocks(xPos,y,n,xPos,y+height,n,block.FENCE)
+		y = mc.getHeight(xPos+size,n)
+        	mc.setBlocks(xPos+size,y,n,xPos+size,y+height,n,block.FENCE)
 
-# Draw a moat around the outside
-def drawMoat(x,y,z):
-
-	#medium = block.WATER
-	medium = block.GOLD_BLOCK
-
-	mc.setBlocks(x,y-1,z,x+32,y-2,z-1,medium)
-	mc.setBlocks(x,y-1,z+32,x+32,y-2,z+33,medium)
-
-	mc.setBlocks(x,y-1,z,x-1,y-2,z+32,medium)
-	#mc.setBlocks(x+32,y-1,z,x+32,y-2,z+32,medium)
-
-def drawFoundations(x,y,z):
+#def drawFoundationsOld(x,y,z):
 
 	# Draw a huge block of stone and clear a huge block or air above it
 
-	mc.setBlocks(x-2,y,z-2,x+33,y-10,z+35,block.MOSS_STONE)
-	mc.setBlocks(x-2,y,z-2,x+33,y+10,z+35,block.AIR)
+#	mc.setBlocks(x-2,y,z-2,x+33,y-10,z+35,block.MOSS_STONE)
+#	mc.setBlocks(x-2,y,z-2,x+33,y+20,z+35,block.AIR)
 
 	# Hoolow out the block to make a huge dungeaon
-	mc.setBlocks(x+1,y-3,z+1,x+32,y-9,z+32,block.AIR)
+#	mc.setBlocks(x+1,y-3,z+1,x+32,y-9,z+32,block.AIR)
 
 	# TODO : what is this? And a hole to get into the cave - 
 	# mc.setBlock(x+10,y,z+10,block.AIR)
 
-def drawFoundationsEx(x,y,z):
+# Draw a set of foundations to build the castle one. thats a block, hollowewed 
+# out to make a cellar, and a moat
+def drawFoundations(x,y,z):
 
 	# Draw a huge block of stone and clear a huge block or air above it
 	mc.setBlocks(x,y,z,x+37,y-10,z+37,block.MOSS_STONE)
-	mc.setBlocks(x,y+1,z,x+37,y+10,z+37,block.AIR)
+	mc.setBlocks(x,y+1,z,x+37,y+20,z+37,block.AIR)
 
 	# Clear a space that will become the moat
 	mc.setBlocks(x+1,y-1,z+1,x+36,y,z+36,block.WATER)
-	#mc.setBlocks(x+2,y-2,z+2,x+36,y-1,z+36,block.AIR)
 
 	# And raise a platform for the castle
 	mc.setBlocks(x+3,y,z+3,x+34,y-1,z+34,block.MOSS_STONE)
@@ -107,10 +124,13 @@ def drawFoundationsEx(x,y,z):
 	# Hollow out the block to make a huge dungeaon
 	mc.setBlocks(x+1,y-3,z+1,x+32,y-9,z+32,block.AIR)
 
+# Get a connection to the game. Work out where the player is
 mc = minecraft.Minecraft.create()
 xPos,yPos,zPos = mc.player.getTilePos()
 
-drawFoundationsEx(xPos-4,yPos,zPos-4)
-drawFence(xPos-3,zPos-3)
-#drawCastle(xPos+2,yPos,zPos)
-#drawMoat(xPos-4,yPos,zPos-4)
+# And draw the castle
+drawFoundations(xPos-4,yPos,zPos-4)
+drawCastle(xPos+2,yPos,zPos)
+drawFenceEx(xPos-5,zPos-5)
+
+
